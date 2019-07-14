9aba4f24fae8ddcb3ad055f82a921084459c3088
github username <github email address>
2019-07-14 13:46:46 +0100

    Palindrome Function

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 //String Reversal
-function reverseString (string) => {
+reverseString = string => {
     return string.split("").reverse.join("")
+}
+
+//Is Palindrome?
+palindrome = string => {
+    let letters = string.split("")
+    let reversed = string.split("").reverse()
+	for(x=0; x < letters.length; x++){
+		if(letters[x].toLowerCase() !== reversed[x].toLowerCase()){
+			return false
+		}
+    }
+	return true
 }
